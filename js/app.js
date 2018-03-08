cf200751eae0bef37d215d5d8e1a9dbd30f56929
“BabaSar” <Baba.Sariffodeen.1@gmail.com>
2018-03-08 20:20:33 +0000

    changed the word button to links
    pagination buttons become pagination links, to make it more in line with project vocab

--- js/app.js
+++ js/app.js
@@ -4,7 +4,16 @@ alert('test');
     //The parent container is <ul class="student-list">, and each student is an <li> child
     //So count how many li tags there are within the ul
 
-const $studentListUl = $('ul.student-list');
-const $studentsNum = $studentListUl.children().length;
+const $studentListUl = $('ul.student-list'); //grabs the ul element
+const $studentsNum = $studentListUl.children().length; //counts how many children
 
 console.log(`There are ${$studentsNum} students.`);
+
+//Based on the number of students, determine how many pagination links we need
+    //Divide by 10, then use Math.ceiling
+let paginationLinksNum = $studentsNum/10;
+paginationLinksNum = Math.ceil(paginationLinksNum);
+
+console.log(paginationLinksNum);
+
+//Insert the required number of pagination links
