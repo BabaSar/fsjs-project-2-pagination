cd30aa739343d2aea31c19588e2633c5051f3dd7
“BabaSar” <Baba.Sariffodeen.1@gmail.com>
2018-03-08 20:04:00 +0000

    determine number of students
    script can count students, based on child elements length within the ul tag, using jquery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 alert('test');
+
+//First find out how many students there are.
+    //The parent container is <ul class="student-list">, and each student is an <li> child
+    //So count how many li tags there are within the ul
+
+const $studentListUl = $('ul.student-list');
+const $studentsNum = $studentListUl.children().length;
+
+console.log(`There are ${$studentsNum} students.`);
